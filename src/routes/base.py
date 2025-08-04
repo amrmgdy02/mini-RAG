c60d2d642fb7e0c36a079256f48ef611139c696f
Amr Magdy <129008521+amrmgdy02@users.noreply.github.com>
2025-08-04 16:09:42 +0300

    response enum

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, APIRouter, Depends
-from helpers.config import get_settings
+from helpers.config import get_settings, Settings
 
 base_router = APIRouter(
     prefix="/api/v1",
@@ -7,7 +7,7 @@ base_router = APIRouter(
 )
 
 @base_router.get("/")
-async def welcome(app_settings=Depends(get_settings)):
+async def welcome(app_settings: Settings =Depends(get_settings)):
     app_name = app_settings.APPLICATION_NAME
     app_version = app_settings.APPLICATION_VERSION
     
